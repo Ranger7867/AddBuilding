61d6fead8be1ca4ba229554c57d46c5e1478fcb0
Zaid <khan_zaid@live.com>
2020-11-10 17:08:52 +1300

    Initial commit

---
+++ Add Building.php
@@ -0,0 +1,14 @@
+<title>Add Building</title>
+<form>
+    First <Nam>Building ID</Nam>e :
+    <input type="text" name="Fname">
+</form>
+<form>
+    First <Nam>Building Name</Nam>e :
+    <input type="text" name="Fname">
+</form>
+<form>
+    First <Nam>Street Address</Nam>e :
+    <input type="text" name="Fname">
+</form>
+<?php
